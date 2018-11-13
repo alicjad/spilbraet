9619887d77ba0d2aced501d001da5e193526f523
Alexandra Caragata <caragata.alexa@yahoo.com>
2018-11-13 17:10:12 +0100

    about, contact, games page created

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -6,7 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Controller {
 
     @GetMapping("/")
-    public String mainpage(){
-        return "index";
+    public String mainPage(){
+        return "mainPage";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(){
+        return "aboutPage";
+    }
+
+    @GetMapping("/games")
+    public String gamesPage(){
+        return "gamesPage";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage(){
+        return"contactPage";
+
     }
 }
