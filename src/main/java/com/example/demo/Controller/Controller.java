ba92f4f8c257d6526b161678a374389148dc6ca0
Alexandra Caragata <caragata.alexa@yahoo.com>
2018-11-13 15:48:26 +0100

    menu realized for the home page

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -1,4 +1,12 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @GetMapping("/")
+    public String mainpage(){
+        return "index";
+    }
 }
